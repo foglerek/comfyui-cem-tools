8c6e65e5f5a4fe1a3f6dcb65823aff97c74a1d2a
Alexander Wesolowski <foglerek@gmail.com>
2023-12-15 12:10:53 -0800

    feat: add batch image processing node

---
+++ nodes/root.py
@@ -0,0 +1,24 @@
+import torch
+
+MAX_RESOLUTION=8192
+
+class ProcessImageBatch:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "reverse": ("BOOLEAN", {"default": False }),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "process"
+    CATEGORY = "CEM"
+
+    def process(self, images, reverse):
+        return torch.cat(list(images).reverse() if reverse else list(images), dim=0)
+
+NODE_CLASS_MAPPINGS = {
+    "ProcessImageBatch": ProcessImageBatch
+}
---
+++ web/cem.js
@@ -0,0 +1,16 @@
+import { app } from '/scripts/app.js'
+import * as root from "./mod_methods/root";
+
+const MOD_METHODS = {
+  ...root
+}
+
+app.registerExtension({
+  name: "cem_tools",
+  async beforeRegisterNodeDef(nodeType, nodeData, app) {
+    MOD_METHODS[nodeData.name]?.beforeDef?.(nodeType, nodeData, app)
+  },
+  nodeCreated(node, app) {
+    MOD_METHODS[node.comfyClass]?.whenCreated?.(node, app)
+  }
+})
